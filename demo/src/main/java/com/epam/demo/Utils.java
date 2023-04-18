fc6206ee21052efbb7efd78354219de1cdee9021
Andrey <98894778+AndreyLevoshenya@users.noreply.github.com>
2023-04-18 17:37:29 +0300

    Add files via upload

--- demo/src/main/java/com/epam/demo/Utils.java
+++ demo/src/main/java/com/epam/demo/Utils.java
@@ -1,13 +1,17 @@
 package com.epam.demo;
 
+import com.epam.utils.StringUtils;
+
 import java.util.List;
+
+import static com.epam.utils.StringUtils.isPositiveNumber;
 
 public class Utils {
     public static boolean isAllPositiveNumbers(List<String> args) {
         if(args != null) {
             if(!args.isEmpty()) {
                 for (String arg : args) {
-                    return arg != null && !arg.isEmpty() && !(Double.parseDouble(arg) <= 0);
+                    isPositiveNumber(arg);
                 }
             }
             else {
