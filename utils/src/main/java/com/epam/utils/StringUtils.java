728efd4d9175e204a21ecaad9d24bc6f7238c692
Andrey <98894778+AndreyLevoshenya@users.noreply.github.com>
2023-04-18 18:39:02 +0300

    Add files via upload

--- utils/src/main/java/com/epam/utils/StringUtils.java
+++ utils/src/main/java/com/epam/utils/StringUtils.java
@@ -6,6 +6,9 @@ public class StringUtils {
         if(str != null) {
             if (!str.isEmpty()) {
                 try {
+                    if(!Character.toString(str.charAt(0)).matches("[1-9]") && str.charAt(1) != '.') {
+                        return false;
+                    }
                     isPositive = Double.parseDouble(str) > 0;
                 }
                 catch (NumberFormatException e) {
